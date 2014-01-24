8d72cd9c163453cf3ea408fedf0112fffc8dbca9
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-01-24 18:30:08 +0000

    Add a second jaws instance
    Former-commit-id: 9b55dfdaf992be549b675384d23557116b892577

--- iocBoot/iocsimple/st.cmd
+++ iocBoot/iocsimple/st.cmd
@@ -5,23 +5,21 @@
 
 < envPaths
 
-epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))SIMPLE"
-epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
-
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase "dbd/simple.dbd"
 simple_registerRecordDeviceDriver pdbbase
 
+< $(IOCSTARTUP)/init.cmd
+
 ## Load record instances
+
+< $(IOCSTARTUP)/dbload.cmd
+
 dbLoadRecords("db/simple.db","P=$(IOCNAME):")
 
-dbLoadRecords("$(TIMESTAMPRECORD)/db/timestamp.db","P=$(IOCNAME):")
-
-dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
-
-pvdump("../pvdump/PV_data.db", "$(IOCNAME):")
+< $(IOCSTARTUP)/preiocinit.cmd
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
